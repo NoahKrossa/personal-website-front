90d35684a338f22493b62638edfc759b6edbbadf
noah krossa <noahkrossa@gmail.com>
2020-10-01 16:40:43 +0200

    REFACTOR: Refactor router

--- src/App.component.tsx
+++ src/App.component.tsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './styles/main.scss'
 import NavegationComponent from './components/Navegation/Navegation.component'
 import Routes from './routes'
 
 export default class AppComponent extends Component {
   render() {
-    return <>
-      <Router>
-        <NavegationComponent/>
-        <div className="container-separator"/>
-        <Routes/>
-      </Router>
-    </> 
+    return (
+      <>
+        <Router>
+          <header>
+            <NavegationComponent />
+          </header>
+          <div className="container-separator" />
+          <main>
+            <Routes />
+          </main>
+        </Router>
+      </>
+    )
   }
 }
