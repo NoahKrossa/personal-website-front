22cf9eb65cdc4d9255a0dbf0655349398b81ff04
Noah Krossa <noahkrossa@gmail.com>
2020-10-13 18:30:28 +0200

    Feat: Create blog view and archivs display component

--- src/components/ArchiveDisplay/ArchiveDisplay.component.tsx
+++ src/components/ArchiveDisplay/ArchiveDisplay.component.tsx
@@ -5,31 +5,16 @@ import { Link } from 'react-router-dom'
 /**
  * TODO: Put interfaces and types in type archive
  */
-type Month =
-  | 'Enero'
-  | 'Febrero'
-  | 'Marzo'
-  | 'Abril'
-  | 'Juno'
-  | 'Julio'
-  | 'Agosto'
-  | 'Septiembre'
-  | 'Octubre'
-  | 'Noviembre'
-  | 'Diciembre'
-
-interface PostRef {
-  title: string
-  id: string
-}
-interface Archives {
-  month: Month
-  posts: PostRef[]
-}
 
 interface ArchiveDisplayComponentProps {
   year: string
-  archives: Archives[]
+  archives: {
+    month: string
+    posts: {
+      title: string
+      id: string
+    }[]
+  }[]
 }
 interface ArchiveDisplayComponentState {
   data: string[]
@@ -51,9 +36,11 @@ export default class ArchiveDisplayComponent extends Component<
                 <span>{e.month}</span>
                 <ul className={styles.archive_month}>
                   {e.posts.map((post, i) => {
-                    ;<li key={i}>
-                      <Link to={`/blog/${post.id}`}>{post.title}</Link>
-                    </li>
+                    return(
+                      <li key={i}>
+                        <Link to={`/blog/${post.id}`}>{post.title}</Link>
+                      </li>
+                    )
                   })}
                 </ul>
               </div>
--- src/views/Blog/Blog.view.tsx
+++ src/views/Blog/Blog.view.tsx
@@ -1,21 +1,45 @@
 import React, { Component, ReactNode } from 'react'
-// import styles from './blog.m.scss'
-
+import styles from './blog.m.scss'
+import PostItemComponent from '../../components/PostItem/PostItem.component'
 /** Component */
-// import ArchiveDisplayComponent from '../../components/ArchiveDisplay/ArchiveDisplay.component'
+import ArchiveDisplayComponent from '../../components/ArchiveDisplay/ArchiveDisplay.component'
+import data from '../../assets/latestPosts.json'
+import postInfo from '../../assets/postsInfo.json'
 
 export default class BlogView extends Component {
   render(): ReactNode {
     return (
       <div className="row">
-        <section className=" col_70__lg col_100__xs">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error
-          doloribus quos aliquam? Impedit deserunt fuga repudiandae nisi eaque
-          quisquam id ex numquam dolores at aliquid, culpa hic, accusantium
-          debitis eum?
+        <section className={styles.section+" "+"col_70__lg col_100__xs"}>
+          {/* Render latest posts */}
+          <h2 className={styles.section_title}>Ultimos articulos</h2>
+          <div className={styles.section_content + ' ' + 'row'}>
+            {data.map((e, i) => {
+              return (
+                <PostItemComponent
+                  key={i}
+                  title={e.title}
+                  description={e.description}
+                  link={e.link}
+                  imgSrc={e.imgSrc}
+                  category={e.category}
+                />
+              )
+            })}
+          </div>
         </section>
-        <aside className="col_30__lg col_70__md col_100__xs">
-          {/* <ArchiveDisplayComponent/> */}
+        <aside className={styles.aside + " " + "col_30__lg col_70__md col_100__xs"}>
+          <h2 className={styles.section_title}>archivos</h2>
+          {postInfo.map((e, i) => {
+            const { year, archives } = e
+            return(
+              <ArchiveDisplayComponent
+                key={i}
+                year={year}
+                archives={archives}
+              />
+            )
+          })}
         </aside>
       </div>
     )
