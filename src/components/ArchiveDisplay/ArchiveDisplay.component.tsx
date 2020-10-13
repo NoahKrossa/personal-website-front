4e8cdf216e0255725a4b265bf279850a10bcc9c1
Noah Krossa <noahkrossa@gmail.com>
2020-10-13 09:51:46 +0200

    Feat: Create archiveDisplay component
    This component is used to list all published articles organized by year and month

---
+++ src/components/ArchiveDisplay/ArchiveDisplay.component.tsx
@@ -0,0 +1,54 @@
+import React, {Component} from 'react'
+import styles from './archiveDisplay.m.scss'
+import {Link} from 'react-router-dom'
+
+/**
+ * TODO: Put interfaces and types in type archive
+ */
+type Month = 'Enero'|'Febrero'|'Marzo'|'Abril'|'Juno'|'Julio'|'Agosto'|'Septiembre'
+|'Octubre'|'Noviembre'| 'Diciembre'
+
+interface PostRef {
+    title:string
+    id:string
+}
+interface Archives {
+    month:Month
+    posts: PostRef[]
+}
+
+interface ArchiveDisplayComponentProps {
+    year:string
+    archives: Archives[]
+}
+
+export default class ArchiveDisplayComponent extends Component<ArchiveDisplayComponentProps, {}> {
+    render() {
+        const {year, archives} = this.props
+        return(
+           <div className={styles.archive_content}>
+               <div className={styles.archive_year}>
+                   <span>{year}</span>
+                   {
+                       archives.map((e, i)=> {
+                           return(
+                            <div key={i} className={styles.archive_list}>
+                                <span>{e.month}</span>
+                                <ul className={styles.archive_month}>
+                                    {
+                                        e.posts.map((post, i)=> {
+                                            <li key={i}>
+                                                <Link to={`/blog/${post.id}`}>{post.title}</Link>
+                                            </li>
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                           )
+                       })
+                   }
+               </div>
+           </div>
+        )
+    }
+}
